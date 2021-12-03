366445c53c125f21526d31186e4eb7fabcc81493
KaterynaKateryna <tok.ar.tok@gmail.com>
2021-12-03 22:26:36 +0100

    day 3 part 1

---
+++ AdventOfCode.Day3/PowerConsumption.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+
+namespace AdventOfCode.Day3;
+
+public class PowerConsumption
+{
+    public async Task<bool[][]> GetInput()
+    {
+        string[] inputRaw = await File.ReadAllLinesAsync("input.txt");
+        return inputRaw.Select(x => ParseBoolArray(x)).ToArray();
+
+        bool[] ParseBoolArray(string input)
+        {
+            return input.Select(x => x == '1').ToArray();
+        }
+    }
+
+    public long CalculatePowerConsumption(bool[][] input)
+    {
+        int numberLength = input[0].Length;
+        int[] zeroes = new int[numberLength];
+        int[] ones = new int[numberLength];
+
+        foreach (bool[] number in input)
+        {
+            for (int i = 0; i < numberLength; ++i)
+            {
+                if (number[i])
+                {
+                    ones[i] += 1;
+                }
+                else
+                {
+                    zeroes[i] += 1;
+                }
+            }
+        }
+
+        bool[] gammaBits = new bool[numberLength];
+        for (int i = 0; i < numberLength; ++i)
+        {
+            gammaBits[i] = ones[i] > zeroes[i];
+        }
+
+        int gammaRateNumber = BoolArrayToInt(gammaBits);
+        int epsilonRateNumber = BoolArrayToInt(gammaBits.Reverse().ToArray());
+
+        return gammaRateNumber * epsilonRateNumber;
+    }
+
+    int BoolArrayToInt(bool[] bits)
+    {
+        int result = 0;
+        for (int i = 0; i < bits.Length; i++)
+        {
+            if (bits[i])
+            {
+                result |= 1 << (bits.Length - i - 1);
+            }
+        }
+        return result;
+    }
+}
---
+++ AdventOfCode.Day3/Program.cs
@@ -0,0 +1,10 @@
+﻿using AdventOfCode.Day3;
+using System.Collections;
+
+// day 3
+PowerConsumption powerConsumption = new PowerConsumption();
+bool[][] input = await powerConsumption.GetInput();
+
+// part 1
+long result = powerConsumption.CalculatePowerConsumption(input);
+Console.WriteLine(result);
