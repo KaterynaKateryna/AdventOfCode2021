881b707fc034011a1b55d894975e67df4f8b595a
KaterynaKateryna <tok.ar.tok@gmail.com>
2021-12-06 19:39:48 +0100

    day 6 part 2

--- AdventOfCode.Day6/Lanternfish.cs
+++ AdventOfCode.Day6/Lanternfish.cs
@@ -2,35 +2,67 @@
 
 public  class Lanternfish
 {
-    public async Task<List<int>> GetInput()
+    public async Task<int[]> GetInput()
     {
         string inputRaw = await File.ReadAllTextAsync("input.txt");
         return inputRaw
             .Split(",", StringSplitOptions.RemoveEmptyEntries)
             .Select(x => int.Parse(x))
-            .ToList();
+            .ToArray();
     }
 
-    public int CountFishAfterDays(List<int> input, int numberOfDays)
+    public int CountFishAfterDaysGreedy(int[] input, int numberOfDays)
     {
+        List<int> result = new List<int>();
+        result.AddRange(input);
+
         for (int i = 0; i < numberOfDays; ++i)
         {
             List<int> newFish = new List<int>();
-            for (int j = 0; j < input.Count; ++j)
+            for (int j = 0; j < result.Count; ++j)
             {
-                if (input[j] == 0)
+                if (result[j] == 0)
                 {
                     newFish.Add(8);
-                    input[j] = 6;
+                    result[j] = 6;
                 }
                 else
                 {
-                    input[j]--;
+                    result[j]--;
                 }
             }
-            input.AddRange(newFish);
+            result.AddRange(newFish);
         }
-        return input.Count;
+        return result.Count;
+    }
+
+    public long CountFishAfterDaysOptimized(int[] input, int numberOfDays)
+    {
+        Dictionary<int, long> fishCounts = new Dictionary<int, long>();
+        foreach (int fish in input)
+        {
+            if (fishCounts.ContainsKey(fish))
+            {
+                fishCounts[fish]++;
+            }
+            else
+            {
+                fishCounts[fish] = 1;
+            }
+        }
+
+        for (int i = 0; i < numberOfDays; ++i)
+        {
+            long numberOfNewFishToAdd = fishCounts.ContainsKey(0) ? fishCounts[0] : 0;
+
+            for (int j = 0; j < 8; ++j)
+            {
+                fishCounts[j] = fishCounts.ContainsKey(j+1) ? fishCounts[j+1] : 0;
+            }
+            fishCounts[8] = numberOfNewFishToAdd;
+            fishCounts[6] += numberOfNewFishToAdd;
+        }
+        return fishCounts.Sum(kv => kv.Value);
     }
 }
 
--- AdventOfCode.Day6/Program.cs
+++ AdventOfCode.Day6/Program.cs
@@ -2,8 +2,12 @@
 
 // day 6
 Lanternfish lanternfish = new Lanternfish();
-List<int> input = await lanternfish.GetInput();
+int[] input = await lanternfish.GetInput();
 
 // part 1
-int numberOfFish = lanternfish.CountFishAfterDays(input, 80);
-Console.WriteLine(numberOfFish);
+int numberOfFish80 = lanternfish.CountFishAfterDaysGreedy(input, 80);
+Console.WriteLine(numberOfFish80);
+
+// part 2
+long numberOfFish256 = lanternfish.CountFishAfterDaysOptimized(input, 256);
+Console.WriteLine(numberOfFish256);
