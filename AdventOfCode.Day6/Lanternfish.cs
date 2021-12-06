b1f4237edbbb01a32916044f05c94381692742c5
KaterynaKateryna <tok.ar.tok@gmail.com>
2021-12-06 19:02:41 +0100

    day 6 part 1

---
+++ AdventOfCode.Day6/Lanternfish.cs
@@ -0,0 +1,36 @@
+﻿namespace AdventOfCode.Day6;
+
+public  class Lanternfish
+{
+    public async Task<List<int>> GetInput()
+    {
+        string inputRaw = await File.ReadAllTextAsync("input.txt");
+        return inputRaw
+            .Split(",", StringSplitOptions.RemoveEmptyEntries)
+            .Select(x => int.Parse(x))
+            .ToList();
+    }
+
+    public int CountFishAfterDays(List<int> input, int numberOfDays)
+    {
+        for (int i = 0; i < numberOfDays; ++i)
+        {
+            List<int> newFish = new List<int>();
+            for (int j = 0; j < input.Count; ++j)
+            {
+                if (input[j] == 0)
+                {
+                    newFish.Add(8);
+                    input[j] = 6;
+                }
+                else
+                {
+                    input[j]--;
+                }
+            }
+            input.AddRange(newFish);
+        }
+        return input.Count;
+    }
+}
+
---
+++ AdventOfCode.Day6/Program.cs
@@ -0,0 +1,9 @@
+﻿using AdventOfCode.Day6;
+
+// day 6
+Lanternfish lanternfish = new Lanternfish();
+List<int> input = await lanternfish.GetInput();
+
+// part 1
+int numberOfFish = lanternfish.CountFishAfterDays(input, 80);
+Console.WriteLine(numberOfFish);
