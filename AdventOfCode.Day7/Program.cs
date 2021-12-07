ca17ac44e1720acfef34b4198518d96edcb1473a
KaterynaKateryna <tok.ar.tok@gmail.com>
2021-12-07 20:34:58 +0100

    day 7 part 1

---
+++ AdventOfCode.Day7/Program.cs
@@ -0,0 +1,9 @@
+﻿using AdventOfCode.Day7;
+
+// day 7
+TheTreacheryOfWhales theTreacheryOfWhales = new TheTreacheryOfWhales();
+int[] input = await theTreacheryOfWhales.GetInput();
+
+// part 1
+int fuel = theTreacheryOfWhales.GetLowestAlignmentFuel(input);
+Console.WriteLine(fuel);
---
+++ AdventOfCode.Day7/TheTreacheryOfWhales.cs
@@ -0,0 +1,35 @@
+﻿namespace AdventOfCode.Day7;
+
+public class TheTreacheryOfWhales
+{
+    public async Task<int[]> GetInput()
+    {
+        string inputRaw = await File.ReadAllTextAsync("input.txt");
+        return inputRaw
+            .Split(",", StringSplitOptions.RemoveEmptyEntries)
+            .Select(x => int.Parse(x))
+            .ToArray();
+    }
+
+    public int GetLowestAlignmentFuel(int[] input)
+    {
+        int fuel = int.MaxValue;
+
+        for (int i = 0; i < input.Length; ++i)
+        {
+            int fuelCandidate = 0;
+
+            for (int j = 0; j < input.Length; ++j)
+            {
+                fuelCandidate += Math.Abs(input[j] - input[i]);
+            }
+
+            if (fuelCandidate < fuel)
+            { 
+                fuel = fuelCandidate;
+            }
+        }
+
+        return fuel;
+    }
+}
