ecf5dc2eec45722c148eef9afbc95ffd7d2d2c72
KaterynaKateryna <tok.ar.tok@gmail.com>
2021-12-15 12:46:17 +0100

    day 14 part 2

--- AdventOfCode.Day14/ExtendedPolymerization.cs
+++ AdventOfCode.Day14/ExtendedPolymerization.cs
@@ -17,7 +17,7 @@ public class ExtendedPolymerization
         return (template, rules);
     }
 
-    public int GetDiffAfterSteps(
+    public int GetDiffAfterStepsGreedy(
         string template,
         Dictionary<string, char> rules,
         int steps
@@ -25,7 +25,7 @@ public class ExtendedPolymerization
     {
         for (int i = 0; i < steps; ++i)
         {
-            template = ApplyStep(template, rules);
+            template = ApplyStepGreedy(template, rules);
         }
 
         Dictionary<char, int> counts = template
@@ -37,7 +37,7 @@ public class ExtendedPolymerization
         return max - min;
     }
 
-    public string ApplyStep(string template, Dictionary<string, char> rules)
+    private string ApplyStepGreedy(string template, Dictionary<string, char> rules)
     {
         StringBuilder sb = new StringBuilder();
         sb.Append(template[0]);
@@ -51,6 +51,81 @@ public class ExtendedPolymerization
             sb.Append(pair[1]);
         }
         return sb.ToString();
+    }
+
+    public long GetDiffAfterStepsOptimized(
+        string template,
+        Dictionary<string, char> rules,
+        int steps
+    )
+    {
+        // initialize pairs count
+        Dictionary<string, long> pairsCount = new Dictionary<string, long>();
+        for (int i = 0; i < template.Length - 1; ++i)
+        {
+            string pair = template.Substring(i, 2);
+            AddCounts(pairsCount, pair, 1);
+        }
+
+        // apply all the steps
+        for (int i = 0; i < steps; ++i)
+        {
+            pairsCount = ApplyStepOptimized(pairsCount, rules);
+        }
+
+        // since pairs overlap, ignore the first character of each pair
+        Dictionary<char, long> lettersCount = new Dictionary<char, long>();
+        foreach (KeyValuePair<string, long> pair in pairsCount)
+        {
+            AddCounts(lettersCount, pair.Key[1], pair.Value);
+        }
+        // except the very first character,
+        // it shouldn't be ignored
+        AddCounts(lettersCount, template[0], 1);
+
+        // get the difference
+        long max = lettersCount.Max(x => x.Value);
+        long min = lettersCount.Min(x => x.Value);
+
+        return max - min;
+    }
+
+    private Dictionary<string, long> ApplyStepOptimized(
+        Dictionary<string, long> pairsCount, 
+        Dictionary<string, char> rules
+    )
+    {
+        Dictionary<string, long> result = new Dictionary<string, long>();
+        foreach (KeyValuePair<string, long> pair in pairsCount)
+        {
+            if (rules.ContainsKey(pair.Key))
+            {
+                char newChar = rules[pair.Key];
+                string newPairLeft = pair.Key[0].ToString() + newChar.ToString();
+                string newPairRight = newChar.ToString() + pair.Key[1].ToString();
+
+                AddCounts(result, newPairLeft, pair.Value);
+                AddCounts(result, newPairRight, pair.Value);
+            }
+            else
+            {
+                result[pair.Key] = pair.Value;
+            }
+        }
+
+        return result;
+    }
+
+    private void AddCounts<TKey>(Dictionary<TKey, long> items, TKey key, long count)
+    {
+        if (items.ContainsKey(key))
+        {
+            items[key] += count;
+        }
+        else
+        {
+            items[key] = count;
+        }
     }
 }
 
--- AdventOfCode.Day14/Program.cs
+++ AdventOfCode.Day14/Program.cs
@@ -5,5 +5,9 @@ ExtendedPolymerization extendedPolymerization = new ExtendedPolymerization();
 (string template, Dictionary<string, char> rules) = await extendedPolymerization.GetInput();
 
 // part 1
-int result = extendedPolymerization.GetDiffAfterSteps(template, rules, 10);
+int result = extendedPolymerization.GetDiffAfterStepsGreedy(template, rules, 10);
 Console.WriteLine(result);
+
+// part 2
+long result2 = extendedPolymerization.GetDiffAfterStepsOptimized(template, rules, 40);
+Console.WriteLine(result2);
